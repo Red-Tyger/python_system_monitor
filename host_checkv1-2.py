873ddfc4f8b2cc3a12b8b62982e17c03c008dc93
Red-Tyger <jmanning.ab@gmail.com>
2025-09-02 21:49:44 -0600

    Added data logging data to CSV file with header

--- host_checkv1-2.py
+++ host_checkv1-2.py
@@ -1,12 +1,12 @@
 """Simple system monitoring tool using the psutil library
 Shows CPU and RAM usage"""
 
-#import the psutil library
+
 import psutil
-#import the time library
 import time
-#import os library
 import os
+import socket
+import datetime
 
 def clear_screen():
     """Clears the screen using ANSI escape sequences"""
@@ -15,9 +15,13 @@ def clear_screen():
 def my_os():
     '''Detects OS script is running on'''
     if os.name == "posix":
-        return ("Posix")
+        return "Posix"
     else:
-        return ("NT")
+        return "NT"
+
+def get_hostname():
+    '''retrieve hostname of computer'''
+    return socket.gethostname()
     
 def byte_format(bytesvalue, suffix="B"):
     """
@@ -26,11 +30,12 @@ def byte_format(bytesvalue, suffix="B"):
     """
     for unit in ["","K","M","G","P","T"]:
         if bytesvalue < 1024:
-            return (f"{bytesvalue:.2f}{unit}{suffix}")
+            return f"{bytesvalue:.2f}{unit}{suffix}"
         bytesvalue /= 1024
 
 def system_performance():
     '''Looks at system and returns performance numbers as a dictionary'''
+    current_host = get_hostname()
     current_os = my_os()
     cpu_usage = psutil.cpu_percent(interval=1)
     ram_usage = psutil.virtual_memory().percent
@@ -40,6 +45,7 @@ def system_performance():
     byte_received = byte_format(net_io.bytes_recv)
     
     return {
+        "current_host" : current_host,
         "current_os" : current_os,
         "cpu_usage" : cpu_usage,
         "ram_usage" : ram_usage,
@@ -50,16 +56,62 @@ def system_performance():
 
 def performance_display(performance_metric):
     '''Accepts a dictionary of performance metrics and displays them'''
+    print(f"Host Name : {performance_metric['current_host']}")
     print(f"OS Platform : {performance_metric['current_os']}")
     print(f"CPU Usage : {performance_metric['cpu_usage']}%")
     print(f"RAM Usage : {performance_metric['ram_usage']}%")
     print(f"Disk Usage : {performance_metric['disk_used']}%")
     print(f"Bytes Sent : {performance_metric['byte_sent']}")
     print(f"Bytes Received : {performance_metric['byte_received']}")
+
+def write_csv_header(log_file_name, performance_metric):
+    '''Check to see if file is new, and if so write a header row for the log data'''
+    #Define the header row contents, based on keys in the performance dictionary
+    header_row = "," .join(performance_metric.keys())
+    #Check to see if log file exists and is not empty
+    if not os.path.exists(log_file_name) or os.stat(log_file_name).st_size == 0:
+        with open(log_file_name, "a") as log_file:
+            log_file.write(f"timestamp,{header_row}\n")
+
+def log_data(performance_metric):
+    '''logs host performance data to a daily CSV file'''
+    #Create a timestamp for the log entry
+    now = datetime.datetime.now()
+    time_stamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    date_string = now.strftime("%Y-%m-%d")
     
-while True:
-    #print the screen escape sequence to clear the screen between loops
-    clear_screen()
-    my_systemdata = system_performance()
-    performance_display(my_systemdata)
-    time.sleep(1)
+    #Define a log file name based on the date
+    log_file_name = f"{performance_metric['current_host']}_{date_string}.csv"
+    
+    #check to see if the log file exists and write the log header row if necessary
+    write_csv_header(log_file_name,performance_metric)
+    
+    #Create the log entry in CSV format
+    log_entry = (
+        f"{time_stamp},"
+        f"{performance_metric['current_host']},"
+        f"{performance_metric['current_os']},"
+        f"{performance_metric['cpu_usage']},"
+        f"{performance_metric['ram_usage']},"
+        f"{performance_metric['disk_used']},"
+        f"{performance_metric['byte_sent']},"
+        f"{performance_metric['byte_received']}\n"
+        )
+        
+    #Write the log entry to the file
+    with open(log_file_name, "a") as log_file:
+        log_file.write(log_entry)
+        
+    
+def main():
+    '''Main function to run the system monitoring application'''    
+    while True:
+        #print the screen escape sequence to clear the screen between loops
+        clear_screen()
+        my_systemdata = system_performance()
+        performance_display(my_systemdata)
+        log_data(my_systemdata)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
