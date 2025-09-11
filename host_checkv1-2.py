7b77d20b806eceb0987c6a81a7ea13abe879101d
Red-Tyger <jmanning.ab@gmail.com>
2025-09-10 18:54:05 -0600

    refactor of code for efficiency

--- host_checkv1-2.py
+++ host_checkv1-2.py
@@ -2,7 +2,6 @@
 """Simple system monitoring tool using the psutil library
 Shows CPU and RAM usage"""
 
-
 import psutil
 import time
 import os
@@ -43,8 +42,8 @@ def system_performance():
     #make a single string of all logged on users
     logged_in = ", ".join(user_names)
     
-    current_host = get_hostname()
-    current_os = my_os()
+    #current_host = get_hostname()
+    #current_os = my_os()
     cpu_usage = psutil.cpu_percent(interval=1)
     ram_usage = psutil.virtual_memory().percent
     disk_used = psutil.disk_usage("/").percent
@@ -69,11 +68,10 @@ def system_performance():
     except Exception as e:
         print(f"Error getting CPU temperature : {e}")
         
-    
     return {
         "logged_in" : logged_in,
-        "current_host" : current_host,
-        "current_os" : current_os,
+        #"current_host" : current_host,
+        #"current_os" : current_os,
         "cpu_usage" : cpu_usage,
         "cpu_temp" : cpu_temp,
         "ram_usage" : ram_usage,
@@ -82,11 +80,11 @@ def system_performance():
         "byte_received" : byte_received
     }
 
-def performance_display(performance_metric):
+def performance_display(performance_metric,current_host,current_os):
     '''Accepts a dictionary of performance metrics and displays them'''
+    print(f"Host Name : {current_host}")
+    print(f"OS Platform : {current_os}")
     print(f"Logged in users: {performance_metric['logged_in']}")
-    print(f"Host Name : {performance_metric['current_host']}")
-    print(f"OS Platform : {performance_metric['current_os']}")
     print(f"CPU Usage : {performance_metric['cpu_usage']}%")
     print(f"CPU Temp : {performance_metric['cpu_temp']}°")
     print(f"RAM Usage : {performance_metric['ram_usage']}%")
@@ -94,16 +92,16 @@ def performance_display(performance_metric):
     print(f"Bytes Sent : {performance_metric['byte_sent']}")
     print(f"Bytes Received : {performance_metric['byte_received']}")
 
-def write_csv_header(log_file_name, performance_metric):
+def write_csv_header(log_file_name, performance_metric,current_host,current_os):
     '''Check to see if file is new, and if so write a header row for the log data'''
     #Define the header row contents, based on keys in the performance dictionary
-    header_row = ",".join(performance_metric.keys())
+    header_row = "current_host" +"," + "current_os" + "," + ",".join(performance_metric.keys())
     #Check to see if log file exists and is not empty
     if not os.path.exists(log_file_name) or os.stat(log_file_name).st_size == 0:
         with open(log_file_name, "a") as log_file:
             log_file.write(f"timestamp,{header_row}\n")
 
-def log_data(performance_metric):
+def log_data(performance_metric,current_host,current_os):
     '''logs host performance data to a daily CSV file'''
     #Create a timestamp for the log entry
     now = datetime.datetime.now()
@@ -111,17 +109,17 @@ def log_data(performance_metric):
     date_string = now.strftime("%Y-%m-%d")
     
     #Define a log file name based on the date
-    log_file_name = f"{performance_metric['current_host']}_{date_string}.csv"
+    log_file_name = f"{current_host}_{date_string}.csv"
     
     #check to see if the log file exists and write the log header row if necessary
-    write_csv_header(log_file_name,performance_metric)
+    write_csv_header(log_file_name,performance_metric,current_host,current_os)
     
     #Create the log entry in CSV format
     log_entry = (
         f"{time_stamp},"
+        f"{current_host},"
+        f"{current_os},"
         f"{performance_metric['logged_in']},"
-        f"{performance_metric['current_host']},"
-        f"{performance_metric['current_os']},"
         f"{performance_metric['cpu_usage']},"
         f"{performance_metric['cpu_temp']},"
         f"{performance_metric['ram_usage']},"
@@ -136,17 +134,25 @@ def log_data(performance_metric):
             log_file.write(log_entry)
     except IOError as e:
         print(f"Failed to write log entry: {e}")
-        
-    
+            
 def main():
-    '''Main function to run the system monitoring application'''    
-    while True:
-        #print the screen escape sequence to clear the screen between loops
-        clear_screen()
-        my_systemdata = system_performance()
-        performance_display(my_systemdata)
-        log_data(my_systemdata)
-        time.sleep(1)
+    '''Main function to run the system monitoring application''' 
+    #retrieve static system information
+    current_host = get_hostname()
+    current_os = my_os()   
+    try:
+        while True:
+            #print the screen escape sequence to clear the screen between loops
+            clear_screen()
+            my_systemdata = system_performance()
+            performance_display(my_systemdata,current_host,current_os)
+            log_data(my_systemdata,current_host,current_os)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Exiting program.")
+        #add a pause so user can confirm program has exited
+        time.sleep(3)
+    
 
 if __name__ == "__main__":
     main()
